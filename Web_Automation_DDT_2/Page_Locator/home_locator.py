096161eddb9a2cabe58387b264d31aa497f93134
yujingmu <41482825+yujingmu@users.noreply.github.com>
2019-07-22 10:46:04 +0800

    Update home_locator.py

--- Web_Automation_DDT_2/Page_Locator/home_locator.py
+++ Web_Automation_DDT_2/Page_Locator/home_locator.py
@@ -4,10 +4,13 @@
 # @Author  : yimi
 # @File    : home_locator.py
 
+from selenium.webdriver.common.by import By
+
 class HomeLocator:
-    user_name_xpath = "//a[@href='/Member/index.html']"
-    first_bid_by_xpath = "//div[@class='b-unit'][1]//span[@class='fs-22']"
-    second_bid_by_xpath = "//div[@class='b-unit'][2]//span[@class='fs-22']"
-    third_bid_by_xpath = "//div[@class='b-unit'][3]//span[@class='fs-22']"
+    user_name_locator = (By.XPATH, "//a[@href='/Member/index.html']")
+    first_bid_locator = (By.XPATH, "//div[@class='b-unit'][1]//span[@class='fs-22']")
+    second_bid_locator = (By.XPATH, "//div[@class='b-unit'][2]//span[@class='fs-22']")
+    third_bid_locator = (By.XPATH, "//div[@class='b-unit'][3]//span[@class='fs-22']")
+    
 
 
